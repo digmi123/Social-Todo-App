71d9db76ccafb8bb3fe98403d3a806820cdfab06
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-07-12 21:50:30 +0300

    Update backend urls.

--- todosApi/todosApi/urls.py
+++ todosApi/todosApi/urls.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/", include("backend.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/auth/", include("backend.urls.auth")),
+    path("api/user/", include("backend.urls.user")),
+    path("api/todos/", include("backend.urls.todos")),
+]
