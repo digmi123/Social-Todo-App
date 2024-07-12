3fa525611cab3351624b686fef1facfd6aa87d74
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-07-12 21:53:14 +0300

    Remove saved model.

--- todosApi/backend/models.py
+++ todosApi/backend/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    # friends = models.ManyToManyField("self", symmetrical=True)
-
     def __str__(self):
         return self.email
 
@@ -31,19 +29,6 @@ class Friend(models.Model):
 
     class Meta:
         unique_together = ("sender", "reciever")
-
-
-class SavedTodos(models.Model):
-    user = models.ForeignKey(
-        User, related_name="adopted_todos", on_delete=models.CASCADE
-    )
-    todo = models.ForeignKey(Todo, related_name="adopters", on_delete=models.CASCADE)
-
-    class Meta:
-        unique_together = ("user", "todo")
-
-    def __str__(self):
-        return f"{self.user.username} adopted {self.todo.title}"
 
 
 class Like(models.Model):
