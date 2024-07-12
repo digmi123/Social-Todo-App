13bf32d13a4c374dfda892c4b15efb35a7f83506
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-07-12 21:59:51 +0300

    Add notification (likes) (still in progress).

--- frontend/src/components/NotificationsBar.jsx
+++ frontend/src/components/NotificationsBar.jsx
@@ -1,19 +1,24 @@
-export default function NotificationsBar() {
-  //   const { loading, userInfo } = useUser();
+import { useUser } from "../providers/UserProvider";
 
-  //   if (loading) return <h1>Loading</h1>;
+export default function NotificationsBar() {
+  const { loading, userInfo } = useUser();
+
+  if (loading) return <h1>Loading</h1>;
 
   return (
-    <aside className="flex flex-col gap-4 border-2 border-slate-200 rounded-md p-4 ">
-      {Array.from({ length: 10 }).map((_, index) => {
+    <aside className="flex flex-col gap-4 rounded-md bg-color-white shadow-box-shadow p-4">
+      {userInfo.notifications.likes.map((like) => {
         return (
           <div
-            key={index}
-            className="flex items-center justify-between gap-4 border-2 border-slate-300 rounded-md p-2"
+            key={like.id}
+            className="flex items-center justify-between gap-4 rounded-md p-2 border-2 border-solid border-slate-200 shadow-box-shadow"
           >
-            <div className="flex gap-4 items-center">
-              <div className="min-w-[40px] min-h-[40px] rounded-full border-2 border-black"></div>
-              <p>Alex (Alex@gmail.com) Liked your todo TODO_TITLE</p>
+            <div className="flex gap-4 items-center py-2 px-1">
+              <span className="material-icons-sharp">account_circle</span>
+              <p>
+                {like.user.username} ({like.user.email}) Liked your todo {""}
+                {`"${like.todo.todo_title}"`}
+              </p>
             </div>
           </div>
         );
