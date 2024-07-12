528d8e28820aa30daa38f63c7fb96f997cb0cdd0
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-07-12 21:58:39 +0300

    Seperate comments into its own component.

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,31 +1,19 @@
-import { useParams } from "react-router-dom";
 import useTodo from "../hooks/useTodo";
 import NotificationsBar from "./NotificationsBar";
-import axios from "axios";
 import Comments from "./Comments";
 import FriendSection from "./FriendsSection";
+import { useParams } from "react-router-dom";
 
 export default function Todo() {
   const { todo_id } = useParams();
-  const { todo, loading } = useTodo({ todoId: todo_id });
-
-  console.log({ todo });
-
-  const handleComment = () => (event) => {
-    event.preventDefault();
-    const data = {
-      todo: todo_id,
-      message: event.target.comment.value,
-    };
-    axios.post("/api/comment_todo/", data);
-  };
-
-  console.log({ todo });
+  const { todo, loading, addComment } = useTodo({
+    todoId: todo_id,
+  });
 
   if (loading) return <h1>Loading</h1>;
 
   return (
-    <div className="grid grid-cols-[1fr_3fr_1.5fr] gap-x-4 p-4 h-full">
+    <div className="grid grid-cols-[1fr_3fr_1.5fr] gap-x-4 p-4 min-h-full">
       <FriendSection />
       <main className="px-4">
         <div className="flex gap-4 items-center">
@@ -38,19 +26,7 @@ export default function Todo() {
           <h4 className="text-2xl">{todo.todo_description}</h4>
         </div>
 
-        <div>
-          <form className="flex gap-4" onSubmit={handleComment()}>
-            <input
-              type="text"
-              name="comment"
-              placeholder="Comment here"
-              className="p-1 border-2 border-slate-300 rounded-md flex-1 px-2 py-2 shadow-box-shadow"
-            />
-            <button type="submit">Comment</button>
-          </form>
-        </div>
-
-        <Comments comments={todo.comments} />
+        <Comments comments={todo.comments} addComment={addComment} />
       </main>
       <NotificationsBar />
     </div>
