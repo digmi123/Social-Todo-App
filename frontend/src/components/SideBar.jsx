5e2b8f3fdcddd2f730d4f7bc74bca5ddf7718c5d
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-07-12 21:59:06 +0300

    Update sideBar component.

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,63 +1,83 @@
-import { useState } from "react";
-import reactLogo from "../assets/react.svg";
+// import { useState } from "react";
+import axios from "axios";
+import { Link, NavLink, useLocation } from "react-router-dom";
+
+const handleLogout = () => {
+  console.log("logout");
+  // axios.post("/api/auth/logout/");
+};
 
 const menuItems = [
   {
-    name: "Dashboard",
+    name: "Feed",
     icon: "dashboard",
+    routePath: "/todos",
   },
   {
     name: "Friends",
     icon: "group",
+    routePath: "friends",
   },
   {
     name: "Notifications",
     icon: "notifications",
+    routePath: "notifications",
   },
   {
-    name: "Settings",
-    icon: "settings",
+    name: "Saved",
+    icon: "bookmarks",
+    routePath: "../saved",
+  },
+  {
+    name: "Favorites",
+    icon: "favorite",
+    routePath: "../favorites",
+  },
+  {
+    name: "Create",
+    icon: "add_circle_outline",
+    routePath: "../create_todo",
   },
 ];
 
 export default function SideBar() {
-  const [active, setActive] = useState(0);
+  // const [active, setActive] = useState(0);
 
   return (
-    <aside className="">
-      {/* <div className="flex items-center justify-between mt-5">
-        <div className="flex gap-2">
-          <img className="w-8 h-8" src={reactLogo} alt="Logo" />
-          <h2>
-            Test<span className="danger">Digmi</span>
-          </h2>
-        </div>
-
-        <div className="pr-4 hidden sm:block">
-          <span className="material-icons-sharp ease-in duration-300 text-base">
-            close
-          </span>
-        </div>
-      </div> */}
-
-      <div className="flex flex-col hover:shadow-none cursor-pointer bg-color-white shadow-box-shadow rounded-xl h-[100vh] top-6 ease-in duration-300">
+    <aside className="flex flex-col justify-between bg-color-white shadow-box-shadow rounded-xl h-full top-6 ease-in duration-300">
+      <div className="flex flex-col hover:shadow-none cursor-pointer">
         {menuItems.map((menuItem) => {
           return (
-            <a
+            <NavLink
               key={menuItem.name}
-              className="flex items-center ms-4 text-color-info-dark h-[3.5rem] gap-4 relative ease-in duration-300 hover:text-color-primary sm:"
+              className="flex items-center ms-4 text-color-info-dark h-[3.5rem] gap-4 relative ease-in duration-300 hover:text-color-primary md:mx-auto"
+              to={`${menuItem.routePath}`}
             >
               <span className="material-icons-sharp ease-in duration-300 text-2xl hover:ml-3">
                 {menuItem.icon}
               </span>
               <div className="sm:inline md:hidden">{menuItem.name}</div>
-            </a>
+            </NavLink>
           );
         })}
-
-        {/* Active class added */}
-        {/* <a className="flex items-center justify-center text-color-info-dark h-[3.5rem] gap-4 relative ease-in duration-300 text-color-primary bg-color-light"> */}
       </div>
+
+      <div className="flex flex-col hover:shadow-none cursor-pointer">
+        <NavLink
+          onClick={handleLogout}
+          key="Logout"
+          to={"../auth/login"}
+          className="flex items-center ms-4 text-color-info-dark h-[3.5rem] gap-4 relative ease-in duration-300 hover:text-color-primary md:mx-auto"
+        >
+          <span className="material-icons-sharp ease-in duration-300 text-2xl hover:ml-3">
+            group
+          </span>
+          <div className="sm:inline md:hidden">Logout</div>
+        </NavLink>
+      </div>
+
+      {/* Active class added */}
+      {/* <a className="flex items-center justify-center text-color-info-dark h-[3.5rem] gap-4 relative ease-in duration-300 text-color-primary bg-color-light"> */}
     </aside>
   );
 }
