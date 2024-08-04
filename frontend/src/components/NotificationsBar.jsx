7536dd42b7c85e56efe8f87871f8b1e67c85c59b
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-08-04 14:48:44 +0300

    Display the notifications.

--- frontend/src/components/NotificationsBar.jsx
+++ frontend/src/components/NotificationsBar.jsx
@@ -6,19 +6,22 @@ export default function NotificationsBar() {
   if (loading) return <h1>Loading</h1>;
 
   return (
-    <aside className="flex flex-col gap-4 rounded-md bg-color-white shadow-box-shadow p-4">
-      {userInfo.notifications.likes.map((like) => {
+    <aside className="flex flex-col gap-4 rounded-md bg-color-white shadow-box-shadow p-4 max-w-96">
+      {userInfo.notifications?.map((notification) => {
         return (
           <div
-            key={like.id}
+            key={notification.id}
             className="flex items-center justify-between gap-4 rounded-md p-2 border-2 border-solid border-slate-200 shadow-box-shadow"
           >
             <div className="flex gap-4 items-center py-2 px-1">
               <span className="material-icons-sharp">account_circle</span>
-              <p>
-                {like.user.username} ({like.user.email}) Liked your todo {""}
-                {`"${like.todo.todo_title}"`}
-              </p>
+              {notification.message ? (
+                <p>
+                  {`${notification.user.email} commented on your post "${notification.message}"`}
+                </p>
+              ) : (
+                <p>{notification.user.email} liked your post</p>
+              )}
             </div>
           </div>
         );
