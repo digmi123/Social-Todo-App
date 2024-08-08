c5f6379f5c9feb16ed31130140bb0230120704ba
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-08-08 10:34:11 +0300

    Wrapping user pages with UserProvider.

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -4,74 +4,82 @@ import Register from "./pages/Register";
 import AuthLayout from "./layouts/AuthLayout";
 import CreateTodo from "./pages/CreateTodo";
 import Todo from "./components/Todo";
-import Todos from "./components/Todos";
+import Todos from "./pages/Todos";
 import FriendSection from "./components/FriendsSection";
 import NotificationsBar from "./components/NotificationsBar";
 import SavedTodos from "./components/SavedTodos";
 import Favorites from "./components/Favorites";
 import EditTodo from "./pages/EditTodo";
+import UserProvider from "./providers/UserProvider";
 
 const router = createBrowserRouter([
   {
-    path: "/todos",
-    element: <Todos />,
+    path: "",
+    element: <UserProvider />,
     children: [
-      { element: <div></div>, index: true },
-      { path: "friends", element: <FriendSection /> },
-      { path: "notifications", element: <NotificationsBar /> },
-      // { path: "/settings", element: <FriendSection /> },
+      {
+        path: "/todos",
+        element: <Todos />,
+        children: [
+          { element: <div></div>, index: true },
+          { path: "friends", element: <FriendSection /> },
+          { path: "notifications", element: <NotificationsBar /> },
+          // { path: "/settings", element: <FriendSection /> },
+        ],
+      },
+      {
+        path: "create_todo",
+        element: <CreateTodo />,
+        children: [
+          { element: <div></div>, index: true },
+          { path: "friends", element: <FriendSection /> },
+          { path: "notifications", element: <NotificationsBar /> },
+          // { path: "/settings", element: <FriendSection /> },
+        ],
+      },
+      {
+        path: "edit_todo/:todo_id",
+        element: <EditTodo />,
+        children: [
+          { element: <div></div>, index: true },
+          { path: "friends", element: <FriendSection /> },
+          { path: "notifications", element: <NotificationsBar /> },
+          // { path: "/settings", element: <FriendSection /> },
+        ],
+      },
+      {
+        path: "todo/:todo_id",
+        element: <Todo />,
+        children: [
+          { element: <div></div>, index: true },
+          { path: "friends", element: <FriendSection /> },
+          { path: "notifications", element: <NotificationsBar /> },
+          // { path: "/settings", element: <FriendSection /> },
+        ],
+      },
+      {
+        path: "favorites",
+        element: <Favorites />,
+        children: [
+          { element: <div></div>, index: true },
+          { path: "friends", element: <FriendSection /> },
+          { path: "notifications", element: <NotificationsBar /> },
+          // { path: "/settings", element: <FriendSection /> },
+        ],
+      },
+      {
+        path: "saved",
+        element: <SavedTodos />,
+        children: [
+          { element: <div></div>, index: true },
+          { path: "friends", element: <FriendSection /> },
+          { path: "notifications", element: <NotificationsBar /> },
+          // { path: "/settings", element: <FriendSection /> },
+        ],
+      },
     ],
   },
-  {
-    path: "create_todo",
-    element: <CreateTodo />,
-    children: [
-      { element: <div></div>, index: true },
-      { path: "friends", element: <FriendSection /> },
-      { path: "notifications", element: <NotificationsBar /> },
-      // { path: "/settings", element: <FriendSection /> },
-    ],
-  },
-  {
-    path: "edit_todo/:todo_id",
-    element: <EditTodo />,
-    children: [
-      { element: <div></div>, index: true },
-      { path: "friends", element: <FriendSection /> },
-      { path: "notifications", element: <NotificationsBar /> },
-      // { path: "/settings", element: <FriendSection /> },
-    ],
-  },
-  {
-    path: "todo/:todo_id",
-    element: <Todo />,
-    children: [
-      { element: <div></div>, index: true },
-      { path: "friends", element: <FriendSection /> },
-      { path: "notifications", element: <NotificationsBar /> },
-      // { path: "/settings", element: <FriendSection /> },
-    ],
-  },
-  {
-    path: "favorites",
-    element: <Favorites />,
-    children: [
-      { element: <div></div>, index: true },
-      { path: "friends", element: <FriendSection /> },
-      { path: "notifications", element: <NotificationsBar /> },
-      // { path: "/settings", element: <FriendSection /> },
-    ],
-  },
-  {
-    path: "saved",
-    element: <SavedTodos />,
-    children: [
-      { element: <div></div>, index: true },
-      { path: "friends", element: <FriendSection /> },
-      { path: "notifications", element: <NotificationsBar /> },
-      // { path: "/settings", element: <FriendSection /> },
-    ],
-  },
+
   {
     path: "auth",
     element: <AuthLayout />,
