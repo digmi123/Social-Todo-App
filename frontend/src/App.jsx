1d75e74bb6c8f081b4ef41802b87bfe968bdc2c5
digmi123 <65775879+digmi123@users.noreply.github.com>
2024-08-08 10:33:31 +0300

    Remove UserProvider from root.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router.jsx";
-import UserProvider from "./providers/UserProvider";
 
 function App() {
-  return (
-    <UserProvider>
-      <RouterProvider router={router} />
-    </UserProvider>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
